60b671207f6d16f1269212ae0bd1a730b4fa4ede
Dan5433 <grishkov.daniil@gmail.com>
2026-02-01 21:28:16 -0500

    Add light switch task trigger functionality

---
+++ Assets/Scripts/Tasks/LightSwitchTask.cs
@@ -0,0 +1,38 @@
+using EditorAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+public class LightSwitchTask : Task
+{
+    [SerializeField] LightSwitch[] lightSwitches;
+    [SerializeField][DisableInEditMode, DisableInPlayMode] List<LightSwitch> disabledSwitches;
+
+    const int MIN_SWITCHES = 1;
+    const float MAX_SWITCHES_RATIO = 0.5f;
+
+    protected override void TriggerInternal()
+    {
+        List<int> switches = new(lightSwitches.Length);
+        for (int i = 0; i < lightSwitches.Length; i++)
+            switches.Add(i);
+
+        float difficulty = TasksManager.Instance.DifficultyFraction;
+
+        float maxSwitches = lightSwitches.Length * MAX_SWITCHES_RATIO;
+
+        int disabledSwitchesAmount = Mathf.RoundToInt(
+            Mathf.Lerp(MIN_SWITCHES, maxSwitches, difficulty));
+
+        for (int i = 0; i < disabledSwitchesAmount; i++)
+        {
+            int randomIndex = Random.Range(0, switches.Count);
+
+            int switchIndex = switches[randomIndex];
+
+            LightSwitch lightSwitch = lightSwitches[switchIndex];
+            lightSwitch.TurnOff();
+            disabledSwitches.Add(lightSwitch);
+
+            switches.Remove(switchIndex);
+        }
+    }
+}

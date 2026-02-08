0a0ab1309ff9f90a2ccef5ad2c58ccf0720d1731
Dan5433 <grishkov.daniil@gmail.com>
2026-02-07 22:42:48 -0500

    Add outline functionality to interactables
    Add outline functionality to interactables by adding a material property block that can change render properties like color and enabled state

--- Assets/Scripts/Interact/Interactable.cs
+++ Assets/Scripts/Interact/Interactable.cs
@@ -1,8 +1,55 @@
+using EditorAttributes;
 using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-    public abstract void Interact();
+    [SerializeField] protected Color outlineColor = Color.white;
+    SpriteRenderer spriteRenderer;
+    MaterialPropertyBlock materialProperties;
 
     protected virtual bool CanInteract => true;
+
+    const string OUTLINE_MATERIAL_NAME = "Sprite Lit Outline";
+    const string OUTLINE_COLOR_PROPERTY_NAME = "_OutlineColor";
+    const string OUTLINE_ENABLED_PROPERTY_NAME = "_OutlineEnabled";
+
+    private void OnValidate()
+    {
+        if (!spriteRenderer)
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        if (materialProperties == null)
+        {
+            materialProperties = new MaterialPropertyBlock();
+            spriteRenderer.GetPropertyBlock(materialProperties);
+        }
+
+        if (!spriteRenderer.sharedMaterial.name.Equals(OUTLINE_MATERIAL_NAME))
+            Debug.LogWarning("SpriteRenderer on Interactable does not have outline material selected!");
+
+        materialProperties.SetColor(OUTLINE_COLOR_PROPERTY_NAME, outlineColor);
+
+        spriteRenderer.SetPropertyBlock(materialProperties);
+    }
+
+    [Button(nameof(RaycastEnter), 36)]
+    public void RaycastEnter()
+    {
+        if (!CanInteract)
+            return;
+
+        materialProperties.SetInteger(OUTLINE_ENABLED_PROPERTY_NAME, 1);
+        spriteRenderer.SetPropertyBlock(materialProperties);
+    }
+
+    [Button(nameof(RaycastExit), 36)]
+    public void RaycastExit()
+    {
+        if (!CanInteract)
+            return;
+
+        materialProperties.SetInteger(OUTLINE_ENABLED_PROPERTY_NAME, 0);
+        spriteRenderer.SetPropertyBlock(materialProperties);
+    }
+    public abstract void Interact();
 }

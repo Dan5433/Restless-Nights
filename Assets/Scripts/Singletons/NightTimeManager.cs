d065a628df2eef8d92324c3cb7ab9e4a5b899ceb
Dan5433 <grishkov.daniil@gmail.com>
2026-02-18 14:11:11 -0500

    Add a max panic threshold
    Add a max panic threshold which controls when max panic is reached

--- Assets/Scripts/Singletons/NightTimeManager.cs
+++ Assets/Scripts/Singletons/NightTimeManager.cs
@@ -3,7 +3,5 @@ using UnityEngine;
 public class NightTimeManager : Singleton<NightTimeManager>
 {
     [SerializeField] int nightDurationSeconds;
-
-    public int NightDurationSeconds => nightDurationSeconds;
     public float NightTimePassedFraction => Time.timeSinceLevelLoad / nightDurationSeconds;
 }

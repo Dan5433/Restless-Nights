5133692eab766fda032b9e0838c5af35c71a7ae7
Dan5433 <grishkov.daniil@gmail.com>
2026-03-18 23:02:51 -0400

    Add win functionality

--- Assets/Scripts/Singletons/NightTimeManager.cs
+++ Assets/Scripts/Singletons/NightTimeManager.cs
@@ -3,5 +3,19 @@ using UnityEngine;
 public class NightTimeManager : Singleton<NightTimeManager>
 {
     [SerializeField] int nightDurationSeconds;
+    [SerializeField] EndTransition endTransition;
     public float NightTimePassedFraction => Time.timeSinceLevelLoad / nightDurationSeconds;
+
+    void Start()
+    {
+        Invoke(nameof(EndNight), nightDurationSeconds);
+    }
+
+    void EndNight()
+    {
+        if (!GameManager.Instance.IsPlaying)
+            return;
+
+        endTransition.Win();
+    }
 }

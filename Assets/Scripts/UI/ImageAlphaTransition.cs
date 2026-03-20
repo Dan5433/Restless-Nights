ecc3851309aa014271ae2dc823957dcc5549a07f
Dan5433 <grishkov.daniil@gmail.com>
2026-03-20 18:09:46 -0400

    Make title screen darkness pulse

--- Assets/Scripts/UI/ImageAlphaTransition.cs
+++ Assets/Scripts/UI/ImageAlphaTransition.cs
@@ -8,9 +8,17 @@ public class ImageAlphaTransition : MonoBehaviour
     [SerializeField] Image image;
     [SerializeField] float easeTime = 1f;
     [SerializeField] float holdTime = 0.25f;
+    [SerializeField] float minAlpha = 0, maxAlpha = 1;
+    [SerializeField] bool playOnStart, loop;
 
     public float EaseTime => easeTime;
     public float HoldTime => holdTime;
+
+    void Start()
+    {
+        if (playOnStart)
+            StartCoroutine(Play());
+    }
 
 #if UNITY_EDITOR
     [Button("Play", 36)]
@@ -28,14 +36,14 @@ public class ImageAlphaTransition : MonoBehaviour
         while (time < easeTime)
         {
             color = image.color;
-            color.a += 1 / (easeTime / Time.deltaTime);
+            color.a = Mathf.Max(Mathf.Lerp(minAlpha, maxAlpha, time / easeTime), color.a);
             image.color = color;
 
             time += Time.deltaTime;
             yield return null;
         }
 
-        color.a = 1;
+        color.a = maxAlpha;
         image.color = color;
 
         yield return new WaitForSeconds(holdTime);
@@ -44,15 +52,17 @@ public class ImageAlphaTransition : MonoBehaviour
         while (time < easeTime)
         {
             color = image.color;
-            color.a -= 1 / (easeTime / Time.deltaTime);
+            color.a = Mathf.Lerp(maxAlpha, minAlpha, time / easeTime);
             image.color = color;
 
             time += Time.deltaTime;
             yield return null;
         }
 
-        color.a = 0;
+        color.a = minAlpha;
         image.color = color;
-    }
 
+        if (loop)
+            StartCoroutine(Play());
+    }
 }
